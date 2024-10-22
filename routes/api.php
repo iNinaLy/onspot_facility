7bd51348dbb56d59cdc2423c3ef8332b4ebc41c5
Husna <116620275+iNinaLy@users.noreply.github.com>
2024-10-22 09:39:24 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,53 +14,6 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Http\Request;
 
 
-// Cleaner Routes
-Route::get('/cleaners', [CleanerController::class, 'index']);
-Route::post('/cleaners', [CleanerController::class, 'store']);
-Route::get('/cleaners/{id}', [CleanerController::class, 'show']);
-Route::put('/cleaners/{id}', [CleanerController::class, 'update']);
-Route::delete('/cleaners/{id}', [CleanerController::class, 'destroy']);
-
-// Officer Routes
-Route::get('/officers', [OfficerController::class, 'index']);
-Route::post('/officers', [OfficerController::class, 'store']);
-Route::get('/officers/{id}', [OfficerController::class, 'show']);
-Route::put('/officers/{id}', [OfficerController::class, 'update']);
-Route::delete('/officers/{id}', [OfficerController::class, 'destroy']);
-
-
-//supervisor
-Route::get('/supervisors', [SupervisorController::class, 'index']);
-Route::post('/supervisors', [SupervisorController::class, 'store']);
-Route::get('/supervisors/{id}', [SupervisorController::class, 'show']);
-Route::put('/supervisors/{id}', [SupervisorController::class, 'update']);
-Route::delete('/supervisors/{id}', [SupervisorController::class, 'destroy']);
-
-
-//complaints
-Route::get('/complaints', [ComplaintController::class, 'index']);
-Route::get('/complaints/{id}', [ComplaintController::class, 'show']);
-Route::put('/complaints/{id}', [ComplaintController::class, 'update']);
-Route::delete('/complaints/{id}', [ComplaintController::class, 'destroy']);
-
-
-//Task
-Route::get('/tasks', [TaskController::class, 'index']);
-Route::post('/tasks', [TaskController::class, 'store']);
-Route::get('/tasks/{id}', [TaskController::class, 'show']);
-Route::put('/tasks/{id}', [TaskController::class, 'update']);
-Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
-
-
-
-
-// TaskCleaner 
-Route::get('/tasks/{id}/cleaners', [TaskCleanerController::class, 'showCleanersForTask']);
-Route::post('/tasks/assign-cleaner', [TaskCleanerController::class, 'assignCleanerToTask']);
-Route::post('/tasks/remove-cleaner', [TaskCleanerController::class, 'removeCleanerFromTask']);
-Route::post('/tasks', [TaskController::class, 'store']);
-
-// Authentication routes
 Route::post('/flutterregister', [AuthController::class, 'register']);
 Route::post('/flutterlogin', [AuthController::class, 'login']);
 Route::post('/flutterregister', [AuthController::class, 'register']); 
